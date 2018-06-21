f3b34de441149839c2d85b6ba1cd60cb74ca0a80
Your Name <inahsanjuan@gmail.com>
2018-06-21 11:02:16 +0800

    Signup, validation, password digest

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
+
+  get '/signup', to: 'users#new'
+
+  resources :users, except: :new
 end
