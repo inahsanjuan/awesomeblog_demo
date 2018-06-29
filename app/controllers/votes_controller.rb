c3202b7166e0408951fa0c9ae75b52b992c2731d
Your Name <inahsanjuan@gmail.com>
2018-06-29 18:08:44 +0800

    Add ajax functionality for vote function

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,19 +1,29 @@
 class VotesController < ApplicationController
   def create
-    micropost = Micropost.find(params[:micropost_id])
+    @micropost = Micropost.find(params[:micropost_id])
     # current_user.votes.create(micropost: micropost)
-    current_user.upvote(micropost)
+    current_user.upvote(@micropost)
 
-    redirect_to request.referrer || root_url
+    # redirect_to request.referrer || root_url
+
+    respond_to do |format|
+      format.html { redirect_to root_url }
+      format.js
+    end
   end
 
   def destroy
-    micropost = Vote.find(params[:id]).micropost
-    current_user.downvote(micropost)
+    @micropost = Vote.find(params[:id]).micropost
+    current_user.downvote(@micropost)
 
-	# vote = Vote.find(params[:id]) 
+    # vote = Vote.find(params[:id]) 
     # vote.destroy
 
-    redirect_to request.referrer || root_url
+    # redirect_to request.referrer || root_url
+
+    respond_to do |format|
+      format.html { redirect_to root_url }
+      format.js
+    end
   end
 end
